f6151ead8780f82687c12fb9873237c7d0a68ac0
IsaacIndex <isaacwongnh@gmail.com>
2023-06-05 21:50:52 +0800

    recreate figma layout

--- src/components/NavProgress.jsx
+++ src/components/NavProgress.jsx
@@ -5,8 +5,21 @@ import Eye from '../assets/eye.svg'
 const NavProgress = (props) => {
     return (
     <div className={styles.NavDiv}>
-      <img src={Eye} /> 
-      <div>{props.progress}</div>
+      <img src={Eye} className={styles.NavIcon} /> 
+      <span className={styles.NavText}>ForseSee</span>
+      <div className={styles.NavProgress}>
+        {/* {props.progress} */}
+
+          <div className={styles.container}>
+            <ul className={styles.progressbar}>
+              <li className={`${styles.progressStep} ${styles.active}`} />
+              <li className={`${styles.progressStep} ${styles.active}`} />
+              <li className={`${styles.progressStep}`} />
+              <li className={`${styles.progressStep}`} />
+              <li className={`${styles.progressStep}`} />
+            </ul>
+          </div>
+      </div>
     </div>
   );
 }
