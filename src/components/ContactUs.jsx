0a8655cb3e7c5f1b5fd8df578254d2ef4ca85960
IsaacIndex <isaacwongnh@gmail.com>
2023-07-23 21:58:50 +0800

    add mobile view

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -9,6 +9,13 @@ import LinkedInQR from "../assets/social media/LinkedIn QR.png"
 import LinktreeQR from "../assets/social media/Linktree QR.png"
 import YouTubeQR from "../assets/social media/YouTube QR.png"
 
+import Facebook from "../assets/social media/Facebook.png"
+import Google from "../assets/social media/Google Play.jpg"
+import Instagram from "../assets/social media/Instagram.png"
+import LinkedIn from "../assets/social media/LinkedIn.png"
+import Linktree from "../assets/social media/Linktree.svg"
+import Youtube from "../assets/social media/Youtube.png"
+
 const ContactUs = forwardRef(function ContactUs(props, ref) {
 
   const [isDesktop, setIsDesktop] = useState(window.innerWidth >= 768);
@@ -18,14 +25,15 @@ const ContactUs = forwardRef(function ContactUs(props, ref) {
       setIsDesktop(window.innerWidth >= 768);
     };
 
-    // Add a resize event listener to update the device type when the window size changes
     window.addEventListener('resize', handleResize);
-
-    // Clean up the event listener when the component is unmounted
     return () => {
       window.removeEventListener('resize', handleResize);
     };
   }, []);
+
+  const handleClick = (url) => {
+    window.open(url, '_blank');
+  };
 
   return (
     <>
@@ -40,33 +48,56 @@ const ContactUs = forwardRef(function ContactUs(props, ref) {
         {isDesktop ? (
           <div className={styles.desktopDiv}>
             <div style={{ "display": "flex", "flexDirection": "column" }}>
-              <img src={InstagramQR} alt="" />
+              <img src={InstagramQR} alt="Instagram" />
               <span>Instagram</span>
             </div>
             <div style={{ "display": "flex", "flexDirection": "column" }}>
-              <img src={FacebookQR} alt="" />
+              <img src={FacebookQR} alt="Facebook" />
               <span>Facebook</span>
             </div>
             <div style={{ "display": "flex", "flexDirection": "column" }}>
-              <img src={GoogleQR} alt="" />
+              <img src={GoogleQR} alt="Google" />
               <span>Google</span>
             </div>
             <div style={{ "display": "flex", "flexDirection": "column" }}>
-              <img src={LinkedInQR} alt="" />
+              <img src={LinkedInQR} alt="LinkedIn" />
               <span>LinkedIn</span>
             </div>
             <div style={{ "display": "flex", "flexDirection": "column" }}>
-              <img src={LinktreeQR} alt="" />
+              <img src={LinktreeQR} alt="Linktree" />
               <span>Linktree</span>
             </div>
             <div style={{ "display": "flex", "flexDirection": "column" }}>
-              <img src={YouTubeQR} alt="" />
+              <img src={YouTubeQR} alt="YouTube" />
               <span>YouTube</span>
             </div>
           </div>
         ) : (
           <div className={styles.mobileDiv}>
-            <img src={FacebookQR} alt="" />
+            <div onClick={() => handleClick('https://instagram.com/foresee_hk?igshid=Y2I2MzMwZWM3ZA==')}>
+              <img src={Instagram} alt="Instagram" />
+              <span>Instagram</span>
+            </div>
+            <div onClick={() => handleClick('https://www.facebook.com/foreseehkust')}>
+              <img src={Facebook} alt="Facebook" />
+              <span>Facebook</span>
+            </div>
+            <div onClick={() => handleClick('https://play.google.com/store/apps/details?id=com.foresee.hk&pli=1')}>
+              <img src={Google} alt="Google Play" />
+              <span>Google Play</span>
+            </div>
+            <div onClick={() => handleClick('https://www.linkedin.com/company/foreseehk/')}>
+              <img src={LinkedIn} alt="LinkedIn" />
+              <span>LinkedIn</span>
+            </div>
+            <div onClick={() => handleClick('https://linktr.ee/foresee_hk?utm_source=qr_code')}>
+              <img src={Linktree} alt="Linktree" />
+              <span>Linktree</span>
+            </div>
+            <div onClick={() => handleClick('https://www.youtube.com/channel/UCXjkhENQuYXNIqz9AdKVZBw')}>
+              <img src={Youtube} alt="Youtube" />
+              <span>Youtube</span>
+            </div>
           </div>
         )
         }
