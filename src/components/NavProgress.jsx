791d0a59a34da359b1aab5a030d1ac8265a539b0
CodingNovi <113091320+CodingNovi@users.noreply.github.com>
2024-02-20 22:05:47 +0800

    made side bar on home page

--- src/components/NavProgress.jsx
+++ src/components/NavProgress.jsx
@@ -3,6 +3,7 @@ import styles from './NavProgress.module.css'
 import Eye from '../assets/eye.svg'
 import MagnifyIcon from 'mdi-react/MagnifyIcon';
 import MenuIcon from 'mdi-react/MenuIcon';
+import CloseIcon from 'mdi-react/CloseIcon';
 import { useState } from "react";
 import MainMenu from "./mainMenu";
 import Sidebar from "react-sidebar";
@@ -65,14 +66,24 @@ class NavProgress extends React.Component {
         </div>
        
       </div>
+      {/* refer to https://github.com/balloob/react-sidebar#readme for sidebar documentation */}
       <Sidebar
-        sidebar={<b>Sidebar content</b>}
+        sidebar={
+          <div><CloseIcon size={30} style={{backgroundColor:'transparent', position:'absolute',right:30,top:30 }} onClick={()=>{this.onSetSidebarOpen(false)}}/>
+        <div className={styles.sidebarLabels}>
+          <h2>Home</h2>
+          <h2>About ForeSee</h2>
+          <h2>Join ForeSee</h2>
+          <h2>Events</h2>
+          <h2>Educational Games</h2>
+          </div>
+        </div>}
         open={this.state.sidebarOpen}
         onSetOpen={this.onSetSidebarOpen}
         pullRight={true}
         styles={{ 
-          sidebar: { background: "white", height:"100%" }, 
-        root:{ backgroundColor:"transparent",height:"fit-content",width:"fit-content",position:"static",marginRight:20},   
+          sidebar: { background: "#B4D9F6", height:"100vh", width:"40%" }, 
+        root:{ backgroundColor:"transparent",height:"fit-content",width:"fit-content",position:"static",marginRight:20, },   
         content:{display:"flex", position:"static", backgroundColor:"transparent", width:"fit-content",  transition: "right .3s ease-out, left .3s ease-out"}
       }}
         
